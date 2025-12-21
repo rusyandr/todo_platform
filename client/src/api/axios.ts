6cd7f684945cfb2c9c16c3262ab8fc32014ee934
rus.agafonov29 <rus.agafonov29@gmail.com>
2025-12-22 00:31:29 +0700

    final fix

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const API_URL =
+  import.meta.env.PROD
+    ? 'https://todo-platform-449e.onrender.com'
+    : 'http://localhost:3000';
+
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use((config) => {
